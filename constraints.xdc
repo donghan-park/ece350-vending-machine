42c1817dd1234e18c49b65764ab754a16d6c7c0b
Donghan Park <donghan.park@duke.edu>
2022-04-01 22:38:39 -0400

    added logic for different product costs

--- constraints.xdc
+++ constraints.xdc
@@ -4,20 +4,23 @@ set_property -dict {PACKAGE_PIN E3 IOSTANDARD LVCMOS33} [get_ports clock];
 # reset
 set_property -dict {PACKAGE_PIN M18 IOSTANDARD LVCMOS33} [get_ports {reset}];
 
+# set_property -dict {PACKAGE_PIN P17 IOSTANDARD LVCMOS33} [get_ports {motor_in1}];
+# set_property -dict {PACKAGE_PIN M17 IOSTANDARD LVCMOS33} [get_ports {motor_in0}];
+# set_property -dict {PACKAGE_PIN D14 IOSTANDARD LVCMOS33} [get_ports {motor_out}];
 
-set_property -dict {PACKAGE_PIN P17 IOSTANDARD LVCMOS33} [get_ports {motor_in1}];
-set_property -dict {PACKAGE_PIN M17 IOSTANDARD LVCMOS33} [get_ports {motor_in0}];
-set_property -dict {PACKAGE_PIN D14 IOSTANDARD LVCMOS33} [get_ports {motor_out}];
 
-# inputs
-set_property -dict {PACKAGE_PIN F13 IOSTANDARD LVCMOS33} [get_ports {in2}];
-set_property -dict {PACKAGE_PIN G13 IOSTANDARD LVCMOS33} [get_ports {in1}];
-set_property -dict {PACKAGE_PIN H16 IOSTANDARD LVCMOS33} [get_ports {in0}];
+set_property -dict {PACKAGE_PIN A13 IOSTANDARD LVCMOS33} [get_ports {buy1_in}];
+set_property -dict {PACKAGE_PIN A15 IOSTANDARD LVCMOS33} [get_ports {buy0_in}];
 
-set_property -dict {PACKAGE_PIN J15 IOSTANDARD LVCMOS33} [get_ports {in_buy}];
-set_property -dict {PACKAGE_PIN E16 IOSTANDARD LVCMOS33} [get_ports {is_insufficient}];
+set_property -dict {PACKAGE_PIN H4 IOSTANDARD LVCMOS33} [get_ports {cost_in3}];
+set_property -dict {PACKAGE_PIN H1 IOSTANDARD LVCMOS33} [get_ports {cost_in2}];
+set_property -dict {PACKAGE_PIN G1 IOSTANDARD LVCMOS33} [get_ports {cost_in1}];
+set_property -dict {PACKAGE_PIN G3 IOSTANDARD LVCMOS33} [get_ports {cost_in0}];
+set_property -dict {PACKAGE_PIN H2 IOSTANDARD LVCMOS33} [get_ports {is_insufficient}];
+set_property -dict {PACKAGE_PIN G4 IOSTANDARD LVCMOS33} [get_ports {in0}];
+set_property -dict {PACKAGE_PIN G2 IOSTANDARD LVCMOS33} [get_ports {in1}];
+set_property -dict {PACKAGE_PIN F3 IOSTANDARD LVCMOS33} [get_ports {in2}];
 
-set_property -dict {PACKAGE_PIN G18 IOSTANDARD LVCMOS33} [get_ports {seg_cat[7]}];
 set_property -dict {PACKAGE_PIN C17 IOSTANDARD LVCMOS33} [get_ports {seg_cat[6]}];
 set_property -dict {PACKAGE_PIN D18 IOSTANDARD LVCMOS33} [get_ports {seg_cat[5]}];
 set_property -dict {PACKAGE_PIN E18 IOSTANDARD LVCMOS33} [get_ports {seg_cat[4]}];
@@ -25,8 +28,22 @@ set_property -dict {PACKAGE_PIN G17 IOSTANDARD LVCMOS33} [get_ports {seg_cat[3]}
 set_property -dict {PACKAGE_PIN D17 IOSTANDARD LVCMOS33} [get_ports {seg_cat[2]}];
 set_property -dict {PACKAGE_PIN E17 IOSTANDARD LVCMOS33} [get_ports {seg_cat[1]}];
 set_property -dict {PACKAGE_PIN F18 IOSTANDARD LVCMOS33} [get_ports {seg_cat[0]}];
+# set_property -dict {PACKAGE_PIN G18 IOSTANDARD LVCMOS33} [get_ports {is_insufficient}];
 
-set_property -dict {PACKAGE_PIN B18 IOSTANDARD LVCMOS33} [get_ports {seg_an[0]}]
-set_property -dict {PACKAGE_PIN B16 IOSTANDARD LVCMOS33} [get_ports {seg_an[1]}]
-set_property -dict {PACKAGE_PIN A15 IOSTANDARD LVCMOS33}  [get_ports {seg_an[2]}]
-set_property -dict {PACKAGE_PIN A13 IOSTANDARD LVCMOS33} [get_ports {seg_an[3]}]
+set_property -dict {PACKAGE_PIN D14 IOSTANDARD LVCMOS33} [get_ports {seg_an[7]}]
+set_property -dict {PACKAGE_PIN F16 IOSTANDARD LVCMOS33} [get_ports {seg_an[6]}]
+set_property -dict {PACKAGE_PIN G16 IOSTANDARD LVCMOS33} [get_ports {seg_an[5]}]
+set_property -dict {PACKAGE_PIN H14 IOSTANDARD LVCMOS33} [get_ports {seg_an[4]}]
+set_property -dict {PACKAGE_PIN E16 IOSTANDARD LVCMOS33} [get_ports {seg_an[3]}]
+set_property -dict {PACKAGE_PIN F13 IOSTANDARD LVCMOS33} [get_ports {seg_an[2]}]
+set_property -dict {PACKAGE_PIN G13 IOSTANDARD LVCMOS33} [get_ports {seg_an[1]}]
+set_property -dict {PACKAGE_PIN H16 IOSTANDARD LVCMOS33} [get_ports {seg_an[0]}]
+
+set_property -dict {PACKAGE_PIN K1 IOSTANDARD LVCMOS33} [get_ports {seg_an[15]}]
+set_property -dict {PACKAGE_PIN F6 IOSTANDARD LVCMOS33} [get_ports {seg_an[14]}]
+set_property -dict {PACKAGE_PIN J2 IOSTANDARD LVCMOS33} [get_ports {seg_an[13]}]
+set_property -dict {PACKAGE_PIN G6 IOSTANDARD LVCMOS33} [get_ports {seg_an[12]}]
+set_property -dict {PACKAGE_PIN E7 IOSTANDARD LVCMOS33} [get_ports {seg_an[11]}]
+set_property -dict {PACKAGE_PIN J3 IOSTANDARD LVCMOS33} [get_ports {seg_an[10]}]
+set_property -dict {PACKAGE_PIN J4 IOSTANDARD LVCMOS33} [get_ports {seg_an[9]}]
+set_property -dict {PACKAGE_PIN E6 IOSTANDARD LVCMOS33} [get_ports {seg_an[8]}]
--- main_module.v
+++ main_module.v
@@ -1,46 +1,51 @@
-module main_module(seg_an, seg_cat, clock, in2, in1, in0, reset, in_buy, is_insufficient, motor_in1, motor_in0, motor_out);
-    input clock, reset, in2, in1, in0, in_buy;
+module main_module(seg_an, seg_cat, clock, in2, in1, in0, reset, buy1_in, buy0_in, is_insufficient, cost_in3, cost_in2, cost_in1, cost_in0);
+    input clock, reset, in2, in1, in0, buy1_in, buy0_in, cost_in3, cost_in2, cost_in1, cost_in0;
     
     reg[31:0] current = 0;
+    reg[31:0] cost1_current, cost0_current;
     reg[31:0] current_digit;
 
     reg[31:0] nickel_change = 1;
     reg[31:0] dime_change = 2;
     reg[31:0] quarter_change = 3;
 
-    output reg[3:0] seg_an;
-    output reg[7:0] seg_cat;
+    output reg[15:0] seg_an;
+    output reg[6:0] seg_cat;
     
-    reg is_decimal;
+    reg[20:0] refresh_counter;
+    wire[3:0] stage_counter;
     
-    reg[19:0] refresh_counter;
-    wire[1:0] stage_counter;
-    
-    output motor_out;
-    input motor_in1, motor_in0;
+    // output motor_out;
+    // input motor_in1, motor_in0;
     
     // debounce logic for coin inputs
     wire db_clock;
     debounce_clock db_clock_module(clock, db_clock);
 
-    wire is_nickel, is_dime, is_quarter, buy_attempted, inc_motor, dec_motor;
+    wire is_nickel, is_dime, is_quarter, buy1_attempted, buy0_attempted; //inc_motor, dec_motor;
+    wire cost1_inc, cost1_dec, cost0_inc, cost0_dec;
     input_db nickel_db(is_nickel, in2, clock, db_clock);
     input_db dime_db(is_dime, in1, clock, db_clock);
     input_db quarter_db(is_quarter, in0, clock, db_clock);
-    input_db buy_db(buy_attempted, in_buy, clock, db_clock);
-    input_db inc_m_db(inc_motor, motor_in1, clock, db_clock);
-    input_db dec_m_db(dec_motor, motor_in0, clock, db_clock);
+    input_db buy1_db(buy1_attempted, buy1_in, clock, db_clock);
+    input_db buy0_db(buy0_attempted, buy0_in, clock, db_clock);
+    input_db cost1_inc_db(cost1_inc, cost_in3, clock, db_clock);
+    input_db cost1_dec_db(cost1_dec, cost_in2, clock, db_clock);
+    input_db cost0_inc_db(cost0_inc, cost_in1, clock, db_clock);
+    input_db cost0_dec_db(cost0_dec, cost_in0, clock, db_clock);
+    // input_db inc_m_db(inc_motor, motor_in1, clock, db_clock);
+    // input_db dec_m_db(dec_motor, motor_in0, clock, db_clock);
     
     // motor stuff
-    reg[7:0] motor_position = 60;
-    always @(posedge clock) begin
-        if(inc_motor)
-            motor_position <= 60;
-        if(dec_motor)
-            motor_position <= 240;
-    end
+    // reg[7:0] motor_position = 60;
+    // always @(posedge clock) begin
+    //     if(inc_motor)
+    //         motor_position <= 60;
+    //     if(dec_motor)
+    //         motor_position <= 240;
+    // end
 
-    servo_controller s_control(clock, reset, motor_position, motor_out);
+    // servo_controller s_control(clock, reset, motor_position, motor_out);
 
     // buy modules
     wire[31:0] cost = 100;
@@ -50,6 +55,7 @@ module main_module(seg_an, seg_cat, clock, in2, in1, in0, reset, in_buy, is_insu
         if(reset) begin
             refresh_counter <= 0;
             current <= 0;
+            is_insufficient <= 1'b0;
         end else
             refresh_counter <= refresh_counter + 1; 
             
@@ -61,53 +67,122 @@ module main_module(seg_an, seg_cat, clock, in2, in1, in0, reset, in_buy, is_insu
             current <= current + 25;
         if(is_nickel | is_dime | is_quarter)
             is_insufficient <= 1'b0; // reset insufficient funds notification if more coins inputted
-        if(buy_attempted & current < cost)
+
+        if(buy1_attempted & current < cost1_current)
             is_insufficient <= 1'b1;
-        if(buy_attempted & current >= cost) begin
+        if(buy1_attempted & current >= cost1_current) begin
             is_insufficient <= 1'b0;
-            current <= current - cost; // update current change
+            current <= current - cost1_current; // update current change
         end
+
+        if(buy0_attempted & current < cost0_current)
+            is_insufficient <= 1'b1;
+        if(buy0_attempted & current >= cost0_current) begin
+            is_insufficient <= 1'b0;
+            current <= current - cost0_current; // update current change
+        end
+
+        if(cost1_inc)
+            cost1_current <= cost1_current + 5;
+        if(cost1_dec && cost1_current >= 5)
+            cost1_current <= cost1_current - 5;
+        if(cost0_inc)
+            cost0_current <= cost0_current + 5;
+        if(cost0_dec && cost0_current >= 5)
+            cost0_current <= cost0_current - 5;
     end
-    assign stage_counter = refresh_counter[19:18];
+    assign stage_counter = refresh_counter[20:17];
     
     always @(*) begin
         case(stage_counter)
-            2'b00: begin
-                seg_an = 4'b0111;
+            // segment 1
+            4'b0000: begin
+                seg_an = 16'b1111111111110111;
                 current_digit = current / 1000;
-                is_decimal <= 1'b0;
             end
-            2'b01: begin
-                seg_an = 4'b1011;
+            4'b0001: begin
+                seg_an = 16'b1111111111111011;
                 current_digit = (current % 1000) / 100;
-                is_decimal <= 1'b1;
             end
-            2'b10: begin
-                seg_an = 4'b1101;
+            4'b0010: begin
+                seg_an = 16'b1111111111111101;
                 current_digit = ((current % 1000) % 100) / 10;
-                is_decimal <= 1'b0;
             end
-            2'b11: begin
-                seg_an = 4'b1110;
+            4'b0011: begin
+                seg_an = 16'b1111111111111110;
                 current_digit = ((current % 1000) % 100) % 10;
-                is_decimal <= 1'b0;
+            end
+
+            // segment 2
+            4'b0100: begin
+                seg_an = 16'b1111111101111111;
+                current_digit = current / 1000;
+            end
+            4'b0101: begin
+                seg_an = 16'b1111111110111111;
+                current_digit = (current % 1000) / 100;
+            end
+            4'b0110: begin
+                seg_an = 16'b1111111111011111;
+                current_digit = ((current % 1000) % 100) / 10;
+            end
+            4'b0111: begin
+                seg_an = 16'b1111111111101111;
+                current_digit = ((current % 1000) % 100) % 10;
+            end
+
+            // segment 3
+            4'b1000: begin
+                seg_an = 16'b1111011111111111;
+                current_digit = cost0_current / 1000;
+            end
+            4'b1001: begin
+                seg_an = 16'b1111101111111111;
+                current_digit = (cost0_current % 1000) / 100;
+            end
+            4'b1010: begin
+                seg_an = 16'b1111110111111111;
+                current_digit = ((cost0_current % 1000) % 100) / 10;
+            end
+            4'b1011: begin
+                seg_an = 16'b1111111011111111;
+                current_digit = ((cost0_current % 1000) % 100) % 10;
+            end
+
+
+            // segment 4
+            4'b1100: begin
+                seg_an = 16'b0111111111111111;
+                current_digit = cost1_current / 1000;
+            end
+            4'b1101: begin
+                seg_an = 16'b1011111111111111;
+                current_digit = (cost1_current % 1000) / 100;
+            end
+            4'b1110: begin
+                seg_an = 16'b1101111111111111;
+                current_digit = ((cost1_current % 1000) % 100) / 10;
+            end
+            4'b1111: begin
+                seg_an = 16'b1110111111111111;
+                current_digit = ((cost1_current % 1000) % 100) % 10;
             end
         endcase
     end
 
     always @(*) begin
         case(current_digit)
-            0: seg_cat = {is_decimal, 7'b1111110};
-            1: seg_cat = {is_decimal, 7'b0110000};
-            2: seg_cat = {is_decimal, 7'b1101101};
-            3: seg_cat = {is_decimal, 7'b1111001};
-            4: seg_cat = {is_decimal, 7'b0110011};
-            5: seg_cat = {is_decimal, 7'b1011011};
-            6: seg_cat = {is_decimal, 7'b1011111};
-            7: seg_cat = {is_decimal, 7'b1110000}; 
-            8: seg_cat = {is_decimal, 7'b1111111};     
-            9: seg_cat = {is_decimal, 7'b1111011};
-            default: seg_cat = {is_decimal, 7'b0000001};
+            0: seg_cat = 7'b1111110;
+            1: seg_cat = 7'b0110000;
+            2: seg_cat = 7'b1101101;
+            3: seg_cat = 7'b1111001;
+            4: seg_cat = 7'b0110011;
+            5: seg_cat = 7'b1011011;
+            6: seg_cat = 7'b1011111;
+            7: seg_cat = 7'b1110000; 
+            8: seg_cat = 7'b1111111;     
+            9: seg_cat = 7'b1111011;
+            default: seg_cat = 7'b0000001;
         endcase
     end
 endmodule
